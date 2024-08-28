650d06c40656f2ed401450ab8ff0ae63b9745aa8
omaduragkar <omaduragkar@gmail.com>
2024-08-28 19:17:27 +0530

    fix: fix last code bug and releasing v1 of the update to made code easy to find
    Adding fix for old code breaking and some stricter type to make visible mandatory.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,47 +1,51 @@
-import { StoryView } from 'react-native-preview-story';
+import { StoryView, type IStoryProp } from 'react-native-preview-story';
 import { Button, SafeAreaView, StatusBar } from 'react-native';
 import styles from '../../src/Styled';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 export default function App() {
   const [visible, setVisible] = React.useState(true);
+  const storyArray: IStoryProp[] = useMemo(
+    () => [
+      {
+        url: 'https://media.giphy.com/media/3o7TKz9b4v8l5ZjGxi/giphy.gif',
+        type: 'image',
+        id: 3,
+        duration: 5,
+      },
+      {
+        url: 'https://media.giphy.com/media/3o7TKz9b4v8l5ZjGxi/giphy.gif',
+        type: 'image',
+        id: 4,
+        duration: 5,
+      },
+      {
+        url: 'https://media.giphy.com/media/3o7TKz9b4v8l5ZjGxi/giphy.gif',
+        type: 'image',
+        id: 5,
+        duration: 5,
+      },
+      {
+        type: 'component',
+        component: <Button title="Hello" onPress={() => {}} />,
+        id: 1,
+        duration: 5,
+      },
+    ],
+    []
+  );
   return (
     <SafeAreaView style={styles.flex1}>
       <StatusBar />
       {visible ? (
         <StoryView
-          stories={[
-            {
-              url: 'https://media.giphy.com/media/3o7TKz9b4v8l5ZjGxi/giphy.gif',
-              type: 'image',
-              id: 3,
-              duration: 5,
-            },
-            {
-              url: 'https://media.giphy.com/media/3o7TKz9b4v8l5ZjGxi/giphy.gif',
-              type: 'image',
-              id: 4,
-              duration: 5,
-            },
-            {
-              url: 'https://media.giphy.com/media/3o7TKz9b4v8l5ZjGxi/giphy.gif',
-              type: 'image',
-              id: 5,
-              duration: 5,
-            },
-            {
-              type: 'component',
-              component: <Button title="Hello" onPress={() => {}} />,
-              id: 1,
-              duration: 5,
-            },
-          ]}
+          stories={storyArray}
           visible
+          storyName="Story Name"
           onComplete={() => {
             setVisible(false);
             console.log('close');
           }}
-          noControls
         />
       ) : (
         <Button title="Show Story" onPress={() => setVisible(true)} />
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import Header from "./components/Header";
-import StoryView from "./components/StoryView";
+import Header from './components/Header';
+import StoryView from './components/StoryView';
 
-export {
-  Header,
-  StoryView
-}
+export { Header, StoryView };
+export type {
+  IContentViewProps,
+  IHeaderProps,
+  IStoryProp,
+  IStoryViewProp,
+} from './types';
